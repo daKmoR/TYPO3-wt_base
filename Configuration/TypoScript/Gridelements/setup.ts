ea9ecaf524e9f7ff87adc61831e632c0c8d1fd20
daKmoR <thomas.allmer@webteam.at>
2012-07-04 14:12:00 +0200

    [BUGFIX] typo in comment

--- Configuration/TypoScript/Gridelements/setup.ts
+++ Configuration/TypoScript/Gridelements/setup.ts
@@ -4,7 +4,7 @@ tt_content.gridelements_pi1.20.10 {
 		# id of the CE grid element
 		1 {
 			columns {
-				# right column with "column number" 1
+				# left column with "column number" 0
 				0 {
 					renderObj =< tt_content
 					wrap = <div class="span6">|</div>
