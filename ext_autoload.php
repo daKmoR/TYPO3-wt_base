72ff8be42ef883156cf8f7bb9e8b7586fe5a72dd
Thomas Allmer <d4kmor@gmail.com>
2013-07-03 11:26:45 +0200

    [FEATURE] add bootstrap as submodule and include css files with Viewhelper directly in the fluid templates

---
+++ ext_autoload.php
@@ -0,0 +1,7 @@
+<?php
+$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('wt_base');
+
+return array(
+	'TYPO3\SiteBase\ViewHelpers\IncludeCssViewHelper' => $extensionPath . 'Classes/ViewHelpers/IncludeCssViewHelper.php',
+);
+?>
